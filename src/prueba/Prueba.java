c3c27278a580577d3ceca25cb373d43ba12f67b7
Giancarlo T.M <Giancarlo T.M@GIANCARLO-LAPTOP>
2024-06-26 17:34:53 -0500

    Prueba3

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -17,6 +17,7 @@ public class Prueba {
         // TODO code application logic here
         System.out.println("Hola Mundo");
         System.out.println("Esto es una prueba");
+        System.out.println("si funciona");
     }
     
 }
